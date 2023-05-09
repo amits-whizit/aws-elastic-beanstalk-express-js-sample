a5d736b392fd59460b8dfb5dcbf26644bbdc31b5
amits-whizit <129951022+amits-whizit@users.noreply.github.com>
2023-05-09 14:25:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Greetings.... Welcome Servosys?'));
+app.get('/', (req, res) => res.send('Thank you sir
+                                    To provide us oportunity to showcase our capabilities ?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
