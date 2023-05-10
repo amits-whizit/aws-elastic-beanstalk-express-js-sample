af3948253b8e298baa12f38b3f098d7c94fe37a4
amits-whizit <129951022+amits-whizit@users.noreply.github.com>
2023-05-10 15:10:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Greetings from WhizIt!, To provide us oportunity to showcase our capabilities'));
+app.get('/', (req, res) => res.send('Greetings from WhizIt
+                                    Thank You to provide us oportunity to showcase our capabilities'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
